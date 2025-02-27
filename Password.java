a9533a28ed090f757b48c525025ee9e1a2fec7ed
Pratham Chavan <pratham1611c@gmail.com>
2025-02-27 23:34:03 +0530

    Update Password.java

--- Password.java
+++ Password.java
@@ -27,6 +27,7 @@ class PasswordTools {
         }
         return false;
     }
+    
     //Prompts the user to enter their password and then verifies it.
     static void verifyPassword(Path path) {
         Scanner reader = new Scanner(System.in);
